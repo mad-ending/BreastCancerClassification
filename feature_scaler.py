433ac7c3b5cbbd339ee75cec29156e41d5b0e6c8
mad-ending <nataliapiasta.wr@gmail.com>
2024-02-12 02:49:21 +0100

    plots and scalers

---
+++ feature_scaler.py
@@ -0,0 +1,15 @@
+from sklearn.preprocessing import StandardScaler, MinMaxScaler, LabelEncoder
+
+def standard_scaler(X_train, X_test):
+    scaler = StandardScaler()
+    X_train = scaler.fit_transform(X_train)
+    X_test = scaler.transform(X_test)
+
+    return X_train, X_test
+
+def min_max_scaler(X_train, X_test):
+    scaler = MinMaxScaler()
+    X_train = scaler.fit_transform(X_train)
+    X_test = scaler.transform(X_test)
+
+    return X_train, X_test
