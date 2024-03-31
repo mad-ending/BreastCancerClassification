ac1266f1af31648f0825aae0c6b6f7f510b810c9
mad-ending <nataliapiasta.wr@gmail.com>
2024-03-31 23:14:59 +0200

    finished project

--- evaluation.py
+++ evaluation.py
@@ -1,19 +1,24 @@
-from sklearn.metrics import accuracy_score, f1_score, recall_score, precision_score, roc_auc_score, roc_curve, confusion_matrix
-# import seaborn as sns
-# import matplotlib.pyplot as plt
+from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score, roc_auc_score, roc_curve, confusion_matrix
+import seaborn as sns
+import matplotlib.pyplot as plt
 
-def evaluate(y_test, y_pred):
+def evaluate(y_test, y_pred, show=False):
     accuracy = accuracy_score(y_test, y_pred)
     f1 = f1_score(y_test, y_pred, average='weighted')
-    recall = recall_score(y_test, y_pred, average='weighted')
     precision = precision_score(y_test, y_pred, average='weighted')
+    recall = recall_score(y_test, y_pred, average='weighted')
     auc = roc_auc_score(y_test, y_pred, average='weighted')
     cm = confusion_matrix(y_test, y_pred)
 
-    # print(f'Accuracy: {accuracy}')
-    # print(f'F1 Score: {f1}')
-    # print(f'Recall: {recall}')
-    # print(f'Precision: {precision}')
-    # print(f'Confusion Matrix: \n{cm}')
+    if show:
+        print(f'Accuracy: {accuracy.round(4)}')
+        print(f'F1 Score: {f1.round(4)}')
+        print(f'Recall: {recall.round(4)}')
+        print(f'Precision: {precision.round(4)}')
+        print(f'AUC: {auc.round(4)}')
+        print(f'Confusion Matrix: \n{cm}')
+
+        sns.heatmap(cm, annot=True, cmap='Blues')
+        plt.show()
 
     return accuracy, f1, recall, precision, auc, cm
